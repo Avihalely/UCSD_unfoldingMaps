f103dd0c2d76c4e1117d6065f9e30d77ffc6ca4d
avihalely <avihalely@gmail.com>
2022-02-07 23:23:52 +0200

    test

--- UCSDUnfoldingMaps/src/module6/OceanQuakeMarker.java
+++ UCSDUnfoldingMaps/src/module6/OceanQuakeMarker.java
@@ -4,7 +4,6 @@ import de.fhpotsdam.unfolding.data.PointFeature;
 import processing.core.PGraphics;
 
 /** Implements a visual marker for ocean earthquakes on an earthquake map
- * 
  * @author UC San Diego Intermediate Software Development MOOC team
  *
  */
